fc2baa164b900eed0f19677c8cc9ee86c81cc960
andremferreira <ferreiraandremendes@gmail.com>
2018-01-11 14:41:54 -0300

    ModificacaoSSL

--- app/config/consts.js
+++ app/config/consts.js
@@ -3,9 +3,9 @@ angular.module('digPront').constant('consts', {
   version: '1.0',
   owner: 'Digital Prontuário',
   year: '2017',
-  site: 'http://digpront.com',
-  apiUrl: 'http://digpront-cnt-br.umbler.net/api',
-  oapiUrl: 'http://digpront-cnt-br.umbler.net/oapi',
+  site: 'https://digpront.com',
+  apiUrl: 'https://digpront.cnt.br/api',
+  oapiUrl: 'https://digpront.cnt.br/oapi',
   // apiUrl: 'http://localhost:3000/api',
   // oapiUrl: 'http://localhost:3000/oapi',  
   userKey: 'digProntApp'
